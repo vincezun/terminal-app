e78565fd7d3b61c567fc5494350a8cb01f0f696d
vinzun <vncnzo@yahoo.com>
2019-05-28 06:13:32 +0800

    Added content

--- src/components/Layout/footer.js
+++ src/components/Layout/footer.js
@@ -1,7 +1,96 @@
 import React from 'react';
+import { Link } from 'gatsby';
+
+import '../../styles/footer.scss';
+import Ok from '../Button/ok';
+import twitterLogo from '../../images/twitter-icon.svg';
+import facebookLogo from '../../images/facebook-icon.svg';
 
 const footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <div className='darkGrayContainer'>
+      <footer className='w-container'>
+        <div className='company'>
+          <h4>COMPANY</h4>
+          <ul className='footerLinks'>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About Terminal</Link>
+            </li>
+            <li>
+              <Link to='/services'>Services</Link>
+            </li>
+            <li>
+              <Link to='/pricing'>Plans & Pricing</Link>
+            </li>
+            <li>
+              <Link to='/'>Blog</Link>
+            </li>
+          </ul>
+        </div>
+        <div className='getHelp'>
+          <h4>GET HELP</h4>
+          <ul className='footerLinks'>
+            <li>
+              <Link to='/'>Knowledge Base</Link>
+            </li>
+            <li>
+              <Link to='/'>Frequently Asked Questions</Link>
+            </li>
+            <li>
+              <Link to='/'>Forum</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact Us</Link>
+            </li>
+          </ul>
+        </div>
+        <div className='boringStuff'>
+          <h4>BORING STUFF</h4>
+          <ul className='footerLinks'>
+            <li>
+              <Link to='/'>Privacy Policy</Link>
+            </li>
+            <li>
+              <Link to='/'>Terms of Service</Link>
+            </li>
+          </ul>
+
+          <div className='newsLetter'>
+            <input
+              type='text'
+              name='email'
+              id='email'
+              className='email'
+              placeholder='Your email'
+            />
+            <Ok className='okBtn' />
+          </div>
+
+          <p>We send about 2 emails a month</p>
+          <a href='https://www.twitter.com'>
+            <img
+              src={twitterLogo}
+              alt='faceboook logo'
+              className='twitter logo'
+            />
+          </a>
+          <a href='https://www.facebook.com'>
+            <img
+              src={facebookLogo}
+              alt='facebook logo'
+              className='facebook logo'
+            />
+          </a>
+          <Link to='/' className='companyLogo'>
+            terminal
+          </Link>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default footer;
