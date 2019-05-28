7076bb1325166bf51f90873c934e49465a3865c2
vinzun <vncnzo@yahoo.com>
2019-05-29 04:18:28 +0800

    Added stuffs

---
+++ src/components/Feature/featureOne.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import '../../styles/featureOne.scss';
+import record from '../../images/record-icon.png';
+import play from '../../images/play-icon.png';
+import pause from '../../images/pause-icon.png';
+
+const featureOne = () => {
+  return (
+    <div className='featureOneWrapper'>
+      <section>
+        <figure>
+          <img src={record} alt='Record icon' className='record icon' />
+          <div>
+            <figcaption id='subHeading'>Record your work.</figcaption>
+            <p>Vivamus sollicitudin, felis vitae facilisis.</p>
+          </div>
+        </figure>
+        <figure>
+          <img src={play} alt='Play icon' className='play icon' />
+          <div>
+            <figcaption id='subHeading'>Play it back.</figcaption>
+            <p>Vivamus sollicitudin, felis vitae facilisis mollis, diam.</p>
+          </div>
+        </figure>
+        <figure>
+          <img src={pause} alt='Pause icon' className='pause icon' />
+          <div>
+            <figcaption id='subHeading'>Pause at any moments.</figcaption>
+            <p>Vivamus sollicitudin, felis vitae facilisis mollis.</p>
+          </div>
+        </figure>
+      </section>
+    </div>
+  );
+};
+
+export default featureOne;
---
+++ src/components/Feature/featureTwo.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import '../../styles/featureTwo.scss';
+import clock from '../../images/clock-icon.png';
+import feather from '../../images/feather-icon.png';
+import send from '../../images/send-icon.png';
+
+const featureTwo = () => {
+  return (
+    <div className='featureTwoWrapper'>
+      <section>
+        <figure>
+          <img src={clock} alt='Clock icon' className='clock icon' />
+          <div>
+            <figcaption id='subHeading'>
+              Track your time on your phone.
+            </figcaption>
+            <p>Vivamus sollicitudin, felis vitae facilisis.</p>
+          </div>
+        </figure>
+        <figure>
+          <img src={feather} alt='Feather icon' className='feather icon' />
+          <div>
+            <figcaption id='subHeading'>
+              Create and edit all your projects.
+            </figcaption>
+            <p>Vivamus sollicitudin, felis vitae facilisis mollis, diam.</p>
+          </div>
+        </figure>
+        <figure>
+          <img src={send} alt='Send icon' className='send icon' />
+          <div>
+            <figcaption id='subHeading'>Send it and get paid.</figcaption>
+            <p>Vivamus sollicitudin, felis vitae facilisis mollis.</p>
+          </div>
+        </figure>
+      </section>
+    </div>
+  );
+};
+
+export default featureTwo;
