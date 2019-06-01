de94b5804e38811b6340eeabef52125d7cb82e95
vinzun <vncnzo@yahoo.com>
2019-06-02 03:10:38 +0800

    Added content

---
+++ src/pages/services.js
@@ -0,0 +1,104 @@
+import React from 'react';
+
+import '../styles/services.scss';
+import Layout from '../components/Layout/layout';
+import TimeTracking from '../components/timeTracking';
+import FeatureOne from '../components/Feature/featureOne';
+import FeatureTwo from '../components/Feature/featureTwo';
+import EmailSupport from '../components/emailSupport';
+
+const services = () => {
+  return (
+    <Layout title='Services | Terminal'>
+      <div className='servicesWrapper'>
+        <section>
+          <div className='w-container'>
+            <div>
+              <div>
+                <p id='heading'>Our Services</p>
+                <p id='description'>
+                  What started as a dorm room project exploded into an internet
+                  sensation!
+                </p>
+              </div>
+              <div className='ourServices'>
+                <div className='service'>
+                  <p className='number'>1.</p>
+                  <p className='serviceTitle'>TIME TRACKING</p>
+                  <p className='description'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse varius enim in eros elementum tristique. 
+                  </p>
+                </div>
+                <div className='service'>
+                  <p className='number'>2.</p>
+                  <p className='serviceTitle'>CLOUD HOSTING</p>
+                  <p className='description'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse varius enim in eros elementum tristique. 
+                  </p>
+                </div>
+                <div className='service'>
+                  <p className='number'>3.</p>
+                  <p className='serviceTitle'>BOOKING</p>
+                  <p className='description'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse varius enim in eros elementum tristique. 
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <TimeTracking heading='Time Tracking' />
+        </section>
+        <section>
+          <div className='w-container'>
+            <div>
+              <FeatureOne />
+            </div>
+            <div>
+              <p className='heading'>Cloud Hosting</p>
+              <p className='description'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse varius enim in eros elementum tristique. Duis
+                cursus, mi quis viverra ornare, eros dolor interdum nulla.
+              </p>
+              <p className='description'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse varius enim in eros elementum tristique. Duis
+                cursus, mi quis viverra ornare, eros dolor interdum nulla.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className='w-container'>
+            <div>
+              <p className='heading'>Booking</p>
+              <p className='description'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse varius enim in eros elementum tristique. Duis
+                cursus, mi quis viverra ornare.
+              </p>
+              <p className='description'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse varius enim in eros elementum tristique. Duis
+                cursus, mi quis viverra ornare.
+              </p>
+            </div>
+            <div>
+              <FeatureTwo />
+            </div>
+          </div>
+        </section>
+        <section>
+          <EmailSupport heading="Also: Rockin' Support " />
+        </section>
+      </div>
+    </Layout>
+  );
+};
+
+export default services;
