d77281604608ad18a4e6bfaee174404ea185da60
vinzun <vncnzo@yahoo.com>
2019-06-04 03:23:22 +0800

    Deleted stuff

--- src/components/Button/sendEmail.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../../styles/sendEmail.scss';
-
-const sendEmail = () => {
-  return <button className='sendEmail'>SEND EMAIL</button>;
-};
-
-export default sendEmail;
