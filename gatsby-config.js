7f8f9fbf6bead8b916fdb9c1002b5dddee2a2120
vinzun <vncnzo@yahoo.com>
2019-06-04 04:58:04 +0800

    Added manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,20 @@ module.exports = {
           })
         ]
       }
-    }
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: `Terminal`,
+        short_name: `Terminal`,
+        start_url: `/`,
+        background_color: `#1d1f21`,
+        theme_color: `#1d1f21`,
+        display: `standalone`,
+        orientation: `landscape`,
+        icon: `src/images/terminal.png`
+      }
+    },
+    `gatsby-plugin-offline`
   ]
 };
