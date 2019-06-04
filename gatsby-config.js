672d72e6c85c47011bcffa8c03d6c5ac3a5739b3
vinczun <vncnzo@yahoo.com>
2019-06-04 21:49:42 +0800

    Removed autoprefixer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,8 @@
-const autoprefixer = require('autoprefixer');
-
 module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
-      resolve: 'gatsby-plugin-sass',
-      options: {
-        postCssPlugins: [
-          autoprefixer({
-            grid: true,
-            flexbox: true
-          })
-        ]
-      }
+      resolve: 'gatsby-plugin-sass'
     },
     {
       resolve: 'gatsby-plugin-manifest',
