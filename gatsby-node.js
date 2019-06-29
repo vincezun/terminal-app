654a613655fb26cbc17f210ecd7a3a0c2495611a
vincezun <vncnzo@yahoo.com>
2019-06-29 15:55:51 +0800

    Deleted stuff

--- gatsby-node.js
+++
@@ -1,7 +0,0 @@
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === 'build-html') {
-    actions.setWebpackConfig({
-      module: { rules: [{ test: /node_modules\/paper/, use: loaders.null() }] }
-    });
-  }
-};
