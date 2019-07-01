1de2f8e5d95dd8a1ce59cdaffcdcc901af680506
vincezun <vncnzo@yahoo.com>
2019-07-01 09:31:11 +0800

    Added form

--- src/components/newsLetter.js
+++ src/components/newsLetter.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'gatsby';
-import { Formik, Field, ErrorMessage } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 import '../styles/newsLetter.scss';
@@ -9,9 +9,6 @@ import facebookLogo from '../images/facebook-icon.svg';
 
 class newsLetter extends Component {
   onSubmit = () => {
-    var okBtn = document.getElementById('okBtn');
-    okBtn.value = 'WAIT...';
-
     setTimeout(() => {
       var subscribe = document.getElementById('subscribe');
       subscribe.style.display = 'none';
@@ -45,61 +42,63 @@ class newsLetter extends Component {
           handleSubmit,
           isSubmitting
         }) => (
-          <div className='newsLetterWrapper'>
-            <div className='subscribe' id='subscribe'>
-              <div>
+          <Form>
+            <div className='newsLetterWrapper'>
+              <div className='subscribe' id='subscribe'>
+                <div>
+                  <Field
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                    type='email'
+                    name='email'
+                    id='email'
+                    className={
+                      errors.email && touched.email ? 'email err' : 'email'
+                    }
+                    placeholder='Your email'
+                  />
+                  <ErrorMessage name='email'>
+                    {msg => <div className='errMsg'>{msg}</div>}
+                  </ErrorMessage>
+                </div>
                 <Field
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.email}
-                  type='email'
-                  name='email'
-                  id='email'
-                  className={
-                    errors.email && touched.email ? 'email err' : 'email'
-                  }
-                  placeholder='Your email'
+                  type='submit'
+                  disabled={isSubmitting}
+                  onClick={handleSubmit}
+                  value='OK'
+                  className='okBtn'
+                  id='okBtn'
                 />
-                <ErrorMessage name='email'>
-                  {msg => <div className='errMsg'>{msg}</div>}
-                </ErrorMessage>
               </div>
-              <Field
-                type='submit'
-                disabled={isSubmitting}
-                onClick={handleSubmit}
-                value='OK'
-                className='okBtn'
-                id='okBtn'
-              />
-            </div>
-            <p id='emailNotif'>We send about 2 emails a month</p>
-            <div className='success' id='success'>
-              <p className='msg'>
-                Thank you! You'll be receiving some awesome emails!
-              </p>
-            </div>
-            <div className='socialLinks'>
-              <a href='https://www.twitter.com'>
-                <img
-                  src={twitterLogo}
-                  alt='faceboook logo'
-                  className='twitter logo'
-                />
-              </a>
-              <a href='https://www.facebook.com'>
-                <img
-                  src={facebookLogo}
-                  alt='facebook logo'
-                  className='facebook logo'
-                />
-              </a>
-            </div>
+              <p id='emailNotif'>We send about 2 emails a month</p>
+              <div className='success' id='success'>
+                <p className='msg'>
+                  Thank you! You'll be receiving some awesome emails!
+                </p>
+              </div>
+              <div className='socialLinks'>
+                <a href='https://www.twitter.com'>
+                  <img
+                    src={twitterLogo}
+                    alt='faceboook logo'
+                    className='twitter logo'
+                  />
+                </a>
+                <a href='https://www.facebook.com'>
+                  <img
+                    src={facebookLogo}
+                    alt='facebook logo'
+                    className='facebook logo'
+                  />
+                </a>
+              </div>
 
-            <Link to='/' className='companyLogo'>
-              terminal
-            </Link>
-          </div>
+              <Link to='/' className='companyLogo'>
+                terminal
+              </Link>
+            </div>
+          </Form>
         )}
       />
     );
